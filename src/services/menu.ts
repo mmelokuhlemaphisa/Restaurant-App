92050e53872daad0f1eb8d4ebebce367aa8763ca
Melokuhle <melokuhlemaphisa99@gmail.com>
2026-01-15 13:16:17 +0200

    feat:Added the homepage to test that the user is able to navigate to home after registing or logging in and i created the menu fetch function.

---
+++ src/services/menu.ts
@@ -0,0 +1,19 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./FireBase";
+
+export interface MenuItem {
+  id: string;
+  name: string;
+  category: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+export const getMenuItems = async (): Promise<MenuItem[]> => {
+  const snapshot = await getDocs(collection(db, "menu"));
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as Omit<MenuItem, "id">),
+  }));
+};
