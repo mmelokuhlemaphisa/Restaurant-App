e9702ed6de53bdaf2f89f6a86b524fea495f8e41
Melokuhle <melokuhlemaphisa99@gmail.com>
2026-01-15 14:57:34 +0200

    Wrap my app in <Provider store={store}>.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "../src/store";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Provider store={store}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </Provider>
+  );
 }
